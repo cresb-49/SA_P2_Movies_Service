7e126784dc861b892d1bb8d384eea1bfa4a5fa8a
Benjamin <carlosbpac@gmail.com>
2025-10-25 15:00:10 -0600

    Add DateUtils import to service applications

--- src/main/java/com/sap/movies_service/MoviesServiceApplication.java
+++ src/main/java/com/sap/movies_service/MoviesServiceApplication.java
@@ -3,14 +3,15 @@ package com.sap.movies_service;
 import com.sap.common_lib.security.web.SecurityConfig;
 import com.sap.common_lib.security.web.WebClientConfig;
 import com.sap.common_lib.security.web.filter.MicroServiceFilter;
+import com.sap.common_lib.util.DateUtils;
 import com.sap.common_lib.util.PublicEndpointUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-//@EnableConfigurationProperties({ AppProperties.class, JwtProperties.class, HeadersProperties.class, PublicEndpointProperties.class })
-@Import({ PublicEndpointUtil.class, SecurityConfig.class, MicroServiceFilter.class, WebClientConfig.class })
+@Import({ PublicEndpointUtil.class, SecurityConfig.class, MicroServiceFilter.class, DateUtils.class, WebClientConfig.class })
 public class MoviesServiceApplication {
 
 	public static void main(String[] args) {
